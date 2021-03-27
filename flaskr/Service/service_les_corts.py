3955c91cd42513f36cf9dc71a6f11d56a44338e6
gemmarevsan <69312176+gemmarevsan@users.noreply.github.com>
2021-03-27 13:28:20 +0100

    service1

---
+++ flaskr/Service/service_les_corts.py
@@ -0,0 +1,24 @@
+from flaskr.Repository.DistrictsRepository import les_corts_rents_repository as lcrr
+import pandas as pd
+from scipy import stats
+import numpy as np
+import statsmodels.api as sm
+from sklearn.preprocessing import StandardScaler
+
+def get_coef_from_training():
+
+    df_les_corts = lcrr.read_csv_les_corts_training()
+
+    scale = StandardScaler()
+    X = df_les_corts[['superficie', 'baños', 'habitaciones']]
+    Y = df_les_corts['precio']
+
+    X[['superficie', 'baños', 'habitaciones']] = scale.fit_transform(X[['superficie', 'baños', 'habitaciones']].values)
+
+    X = sm.add_constant(X)
+    est = sm.OLS(Y, X).fit()
+    params = []
+    for param in est.params:
+        params = params.append(param)
+
+    return params
