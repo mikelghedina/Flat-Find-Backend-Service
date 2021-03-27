844bfa9c0853e018e00e0f288810cc267b083ee1
mikelghedina <mikel.ghedina@gmail.com>
2021-03-27 11:16:15 +0100

    import dataset

--- flaskr/Repository/TrainingAverageRentsRepository.py
+++ flaskr/Repository/TrainingAverageRentsRepository.py
@@ -3,17 +3,19 @@ import json
 
 
 def read_csv_average_training():
-    df_eixample = pd.read_csv('./datos/.csv')
+    df_eixample = pd.read_csv('./flaskr/datos/datasets_Pisos.com/alquiler_ciutat_vella(limpio) - alquiler_ciutat_vella.csv')
     return df_eixample
 
-
-def create_csv_average_training(data, file):
-    with open('trained_average_rents.json', 'w') as file:
-        json.dump(data, file, indent=4)
+print(read_csv_average_training())
 
 
-def read_csv_average_trained():
-    with open('trained_average_rents.json') as file:
-        trained_data_rents = json.load(file)
-
-    return trained_data_rents
+# def create_csv_average_training(data, file):
+#     with open('trained_average_rents.json', 'w') as file:
+#         json.dump(data, file, indent=4)
+#
+#
+# def read_csv_average_trained():
+#     with open('trained_average_rents.json') as file:
+#         trained_data_rents = json.load(file)
+#
+#     return trained_data_rents
