1cc70761aacd965eb5942a03543b9ca47a11afcf
mikelghedina <mikel.ghedina@gmail.com>
2021-03-29 14:02:11 +0200

    flask api init

--- flaskr/main.py
+++ flaskr/main.py
@@ -1,10 +1,16 @@
 import NewService as s
 import Flask 
 
+app = Flask(__name__)
+
+
 coefsDict = s.get_coefs_by_district("Eixample")
 
 #Dictionario de coeficientes, N de m2, N de Baños, N de habs
-estimated_price = s.model_inference(coefsDict,80,1,3)
+estimated_price = 
 print(estimated_price)
 
+@app.route('/est/<int:sup>/<int:baths>/<int:rooms>')
+def estimated_price(sup, baths, rooms):
+    return s.model_inference(coefsDict,sup,baths,rooms)
 
