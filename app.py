0a8e2e236e261432325f44abfc7d27db6a371649
mikelghedina <mikel.ghedina@gmail.com>
2021-03-29 17:51:24 +0200

    2 routes work

--- app.py
+++ app.py
@@ -5,14 +5,14 @@ import math
 app = Flask(__name__)
 
 
-coefsDict = s.get_coefs_by_district("Eixample")
+# coefsDict = s.get_coefs_by_district("Eixample")
 
 #Dictionario de coeficientes, N de m2, N de Baños, N de habs
 # print(s.model_inference(coefsDict,sup,baths,rooms))
 
-@app.route("/est/<int:sup>-<int:baths>-<int:rooms>", methods=["GET"])
-def estimated_price(sup,baths,rooms):
-    price = s.model_inference(coefsDict,sup,baths,rooms)
+@app.route("/est/<string:district_name>-<int:sup>-<int:baths>-<int:rooms>", methods=["GET"])
+def estimated_price(district_name,sup,baths,rooms):
+    price = s.model_inference(s.get_coefs_by_district(district_name),sup,baths,rooms)
     return jsonify({"estimed price": round(price, 2)})
 
 
